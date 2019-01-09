69dfeaa71dd61b114d175a22994ca14776fdfd5d
Sy Hung <nguyensyhung689@gmail.com>
2019-01-09 19:03:11 +0700

    Photo editing display, user profile

--- resources/views/account/addresses.blade.php
+++ resources/views/account/addresses.blade.php
@@ -47,7 +47,12 @@
                 <div class="ctAccount">
                     <div class="detailAccount bg_w">
                         <div class="accountAddress">
-                            <h3> Sổ địa chỉ nhận hàng: </h3>
+                            <h3> Sổ địa chỉ nhận hàng: 
+                                <a href="{{route('account.address.create')}}"  title="Thêm địa chỉ mới" class="logout open_add">
+                                    <i class="fa fa-book" aria-hidden="true"></i>
+                                     Thêm địa chỉ mới
+                                </a>
+                            </h3>
                             <div class="address row">
                                 <div class="col-sm-6 co-xs-12 address_defaul">
                                     <div class="main">
@@ -60,9 +65,15 @@
                                                     <p class="address"> Tỉnh/Thành phố: {{$address->city->name}}</p>
                                                     <p class="address"> Địa chỉ cụ thể: {{$address->address}}</p>  
                                                     <p class="phone"> Số điện thoại: {{$address->phone}}</p>
-                                                    <p class="address_actions">
-                                                    <span class="action_link action_edit"><a href="{{route('address.edit', ['id' => $address->id ])}}">Sửa</a></span>
-                                                        <span class="action_link action_delete"><a href="">Xóa</a></span>
+                                                    <p class="address_actions pull-left">
+                                                        <span class="action_link action_edit"><a href="{{route('address.edit', ['id' => $address->id ])}}">Sửa</a></span>
+                                                        <span class="action_link action_delete">
+                                                            <form action="{{route('account.address.delete', $address->id)}}" method="POST">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                                    <button type="submit" class="btn btn-light">Delete</button>
+                                                                </form>
+                                                        </span>
                                                     </p>
                                                     @endforeach
                                                 </div>
